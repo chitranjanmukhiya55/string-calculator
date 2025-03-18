b670b919340ef2de546f42015436797ae8ccbe73
chitranjan <143383243+chitranjanmukhiya55@users.noreply.github.com>
2025-03-17 19:02:57 -0700

    return empty return file

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -1,3 +1,5 @@
+import {add} from './stringCalculator'
+
 test('Empty string returns 0', () => {
     expect(add('')).toBe(0);
 });
---
+++ stringCalculator.ts
@@ -0,0 +1,3 @@
+export function add(numbers: string): number {
+    return 0; // To make the test pass
+}
