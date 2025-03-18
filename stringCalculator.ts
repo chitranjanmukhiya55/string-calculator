7bd35f351a370fcf1a8433281c75f55941bc9589
chitranjan <143383243+chitranjanmukhiya55@users.noreply.github.com>
2025-03-17 19:07:10 -0700

    Two numbers returns their sum => passed

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,4 +1,5 @@
 export function add(numbers: string): number {
     if (numbers === '') return 0;
-    return parseInt(numbers, 10);
+    const numArray = numbers.split(',').map(Number);
+    return numArray.reduce((sum, num) => sum + num, 0);
 }
